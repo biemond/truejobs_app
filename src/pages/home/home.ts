84138fcfc24aeecba923e5ae9fe92acd9a158b51
Edwin Biemond <biemond@gmail.com>
2018-01-23 18:38:44 +0530

    update with page links

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { JobItemPage } from '../job-item/job-item';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: string = 'https://www.googleapis.com/blogger/v3/blogs/6590972831374935792/posts?key=AIzaSyBdtXGJesZvK6p3jPCd6JFcVcf9gsTYEbQ&fetchImages=true&status=live&view=READER'; //&fetchBodies=false';
+  items: any;
 
+  constructor(public navCtrl: NavController,
+              private http: Http,
+              public platform: Platform) {
+    platform.ready().then(() => {
+      this.refreshData();
+    });
+  }
+
+  refreshData(){
+    console.log('RefreshData');
+    this.http.get(this.url)
+      .map(res => res.json())
+      .subscribe(data => {
+          this.items = data.items;
+          console.log(this.items);
+      });
+  }
+
+  itemTapped(event, item) {
+      this.navCtrl.push(JobItemPage, { item: item });
   }
 
 }
---
+++ src/pages/job-item/job-item.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-job-item',
+  templateUrl: 'job-item.html',
+})
+export class JobItemPage {
+
+  selectedItem: any;
+
+  constructor(private nav: NavController,
+              private navParams: NavParams) {
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+  }
+
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad JobItemPage');
+  }
+
+}
