b5e6bc131f79db981560a6797d1b72cb6a712206
Edwin Biemond <biemond@gmail.com>
2018-01-28 12:50:00 +0530

    get all items

--- src/providers/blogger/blogger.ts
+++ src/providers/blogger/blogger.ts
@@ -9,39 +9,65 @@ export class BloggerProvider {
 
   urlPost: string = 'https://www.googleapis.com/blogger/v3/blogs/6590972831374935792/posts/'
   keyPost: string = '?key='+GlobalVariable.API_KEY+'&fetchImages=true';
-
+  items: any;
 
   constructor(public http: HttpClient) {
   }
 
   getAllJobs() {
     console.log("getAllJobs");
+    this.items = [];
     return new Promise((resolve, reject) => {
       this.http.get(this.url)
         .subscribe(data => {
-          // console.log(data);
-          var nextToken = data['nextPageToken'];
-          console.log("nexttoken" + nextToken);
-          // console.log(data['items']);
-          resolve(data['items']);
+          this.items = data['items'];
+          if (data['nextPageToken'] != null ) {
+            this.getAllJobsNextSet(data['nextPageToken']);
+          }  
+          resolve(this.items);
         }, (err) => {
           reject(err);
         });
     });
   }
 
+  getAllJobsNextSet(token) {
+    console.log("getAllJobsNextSet "+ token);
+    this.http.get(this.url + "&pageToken="+token)
+      .subscribe(data => {
+        this.items.push(data['items']);
+        if (data['nextPageToken'] != null) {
+          this.getAllJobsNextSet(data['nextPageToken']);
+        } 
+      });
+  }
+
   getAllJobsByLabel(label) {
     console.log("getAllJobsByLabel " + label);
+    this.items = [];
     return new Promise((resolve, reject) => {
       this.http.get(this.url + "&labels=" + label)
         .subscribe(data => {
-          // var nextToken = data['nextPageToken'];
-          // console.log("nexttoken" + nextToken);
-          resolve(data['items']);
+          this.items = data['items'];
+          if (data['nextPageToken'] != null ) {
+            this.getAllJobsByLabelNextSet(label,data['nextPageToken']);
+          }  
+          resolve(this.items);
         }, (err) => {
           reject(err);
         });
     });
+  }
+
+  getAllJobsByLabelNextSet(label, token) {
+    console.log("getAllJobsByLabelNextSet "+ token);
+    this.http.get(this.url + "&labels=" + label + "&pageToken="+token)
+      .subscribe(data => {
+        this.items.push(data['items']);
+        if (data['nextPageToken'] != null) {
+          this.getAllJobsByLabelNextSet(label,data['nextPageToken']);
+        } 
+      });
   }
 
   getPostById(id) {
